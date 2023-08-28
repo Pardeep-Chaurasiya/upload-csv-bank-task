8cbe0b89f952c7991ed86203028f0698e4e99d38
Pardeep Chaurasiya <chaurasiyapardeep001@gmail.com>
2023-08-28 14:35:16 +0530

    bank task completed

--- routes/bankRoute.js
+++ routes/bankRoute.js
@@ -4,6 +4,7 @@ const bankController = require("../controller/bankController");
 
 const router = express.Router();
 
+router.post("/login", bankController.login);
 router.post("/uploadhdfc", upload.single("file"), bankController.hdfc);
 router.post("/uploadicici", upload.single("file"), bankController.icici);
 
